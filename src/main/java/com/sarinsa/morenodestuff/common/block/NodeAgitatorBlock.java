4e722553dbb1d71321bab309b91201d47db8a111
Sarinsa <mikkemusbruker@gmail.com>
2025-12-30 15:01:23 +0100

    Fixed break particle for Node Agitator

--- src/main/java/com/sarinsa/morenodestuff/common/block/NodeAgitatorBlock.java
+++ src/main/java/com/sarinsa/morenodestuff/common/block/NodeAgitatorBlock.java
@@ -2,9 +2,13 @@ package com.sarinsa.morenodestuff.common.block;
 
 import com.sarinsa.morenodestuff.common.Util;
 import com.sarinsa.morenodestuff.common.tileentity.NodeAgitatorTileEntity;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class NodeAgitatorBlock extends BlockContainer {
@@ -31,5 +35,11 @@ public class NodeAgitatorBlock extends BlockContainer {
     @Override
     public int getRenderType() {
         return Util.BlockRenderID.NODE_AGITATOR;
+    }
+    
+    @SideOnly( Side.CLIENT )
+    @Override
+    public void registerBlockIcons( IIconRegister iconRegister ) {
+        blockIcon = iconRegister.registerIcon( new ResourceLocation( "thaumcraft", "arcane_stone" ).toString() );
     }
 }
